98a30fd6d18092b9b88cdf62b40b443d1225e632
ROB GILLEN <rob@graphicsfactory.com>
2018-06-20 21:30:21 -0600

    list complete with data source and delegate methods.

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by ROB GILLEN on 6/20/18.
+//  Copyright © 2018 Graphics Factory. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    
+    let itemArray = ["Make a Song","Make an App", "Make a 3D scene"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        //
+        
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+}
+
