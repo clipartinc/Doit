eb8a05676684596cb31d3f117ae9d6dca05ed03f
ROB GILLEN <rob@graphicsfactory.com>
2018-06-20 23:02:28 -0600

    saving data using user defaults

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -13,9 +13,15 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Make a Song","Make an App", "Make a 3D scene"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview Methods
@@ -67,6 +73,8 @@ class ToDoListViewController: UITableViewController {
             //print(textField.text)
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             
         }
